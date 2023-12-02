504be32585acc397ecaee1b2f1ae6c394a107506
Cosmits <salge.cos@gmail.com>
2023-12-02 11:28:27 +0100

    add lockfile

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 import tokens from './models/env.js'
 import TelegramBot from 'node-telegram-bot-api';
 import { startServiceBot } from './mainService.js';
+import fs from 'fs';
 
-let bot = null;
+const lockfile = './My_bot.lock';
 
-async function start() {
-    bot = new TelegramBot(tokens.TELEGRAM_TOKEN);
-    await bot.startPolling({ restart: true });
-    startServiceBot(bot)
-}
-
-async function stop() {
-    if (bot != null) {
-        await bot.stopPolling({ cancel: true });
-        bot = null;
+async function checkIfRunning() {
+    if (fs.existsSync(lockfile)) {
+        console.log('Копія додатку вже запущена. Зупиняємо цю копію.');
+        process.exit();
+    } else {
+        fs.writeFileSync(lockfile, process.pid);
+        console.log('Додаток запущено.');
+        const My_bot = new TelegramBot(tokens.TELEGRAM_TOKEN);
+        await My_bot.startPolling({ restart: true });
+        startServiceBot(My_bot)
     }
-    process.exit();
 }
 
-start();
-process.on('SIGQUIT', stop);
-process.on('SIGINT', stop);
+process.on('exit', function () {
+    fs.unlinkSync(lockfile);
+});
+
+checkIfRunning();
